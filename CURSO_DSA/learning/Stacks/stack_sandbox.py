303606982fbd1d78f7cf311421a0030447f22eaa
Alejandro Cruz Martinez <230110322@itsoeh.edu.mx>
2024-12-10 22:31:55 -0600

    se completo el test 2 de stacks
    Signed-off-by: Alejandro Cruz Martinez <230110322@itsoeh.edu.mx>

--- CURSO_DSA/learning/Stacks/stack_sandbox.py
+++ CURSO_DSA/learning/Stacks/stack_sandbox.py
@@ -4,9 +4,17 @@ class Stack:
     def __init__(self):
         self.top_item = None
     def push(self, value):
-        new_node = Node(value)
-        new_node.set_next_node(self.top_item)  # Establece el siguiente nodo
-        self.top_item = new_node  # Actualiza el nodo superior
+        item = Node(value)  # Crear un nuevo nodo
+        item.set_next_node(self.top_item)  # Establecer el siguiente nodo
+        self.top_item = item  # Actualizar el top_item
+    def pop(self):
+        if self.top_item is None:
+            logging.warning("No se puede hacer pop en una pila vacía.")
+            raise AttributeError("No se puede hacer pop en una pila vacía.")
+        item_to_remove = self.top_item  # Guardar el item a eliminar
+        self.top_item = item_to_remove.next  # Actualizar el top_item
+        return item_to_remove.get_value()
+    
     def peek(self):
         if self.top_item is not None:
             return self.top_item.get_value()
